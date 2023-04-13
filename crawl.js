c0b97094b167eb3d3bab7b4c06c5d786d4e95f6a
DVastMasters <contatorafaeldutra@outlook.com>
2023-04-13 09:45:59 -0300

    implementing crawlPage function

--- crawl.js
+++ crawl.js
@@ -30,7 +30,25 @@ const getURLsFromHTML = (htmlBody, baseURL) => {
 	return urls;
 } 	
 
+const crawlPage = async (base_url) => {
+	try {
+		const response = await fetch(base_url);
+		if (!response.ok) {
+			throw new Error("Network error. HTTP response isn't 200.");
+		};
+		
+		if (!response.headers.get("Content-Type").includes('text/html')) {
+			throw new Error("Content-Type isn't text/html.");
+		}
+
+		response.text().then(text => console.log(text));
+	} catch (error) {
+		console.log('Error: ' + error);
+	}
+}
+
 module.exports = {
 	normalizeURL,
-	getURLsFromHTML
+	getURLsFromHTML,
+	crawlPage
 }
